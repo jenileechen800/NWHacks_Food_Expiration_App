f9e6f6dc5c48ec43b20894ebdabb4a13f4956dcf
jenileechen800 <chen.jenilee@gmail.com>
2020-01-11 14:32:40 -0800

    first commit

---
+++ app/src/main/java/com/hfad/nwhacks_food_expiration_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hfad.nwhacks_food_expiration_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
