865059809e1ad6bbae2b76afdc6b2fdd08a8678a
mpchow <mattchow918@gmail.com>
2020-01-11 15:40:20 -0800

    FoodItem Class

---
+++ app/src/main/java/com/hfad/nwhacks_food_expiration_app/FoodItem.java
@@ -0,0 +1,31 @@
+package com.hfad.nwhacks_food_expiration_app;
+
+public class FoodItem {
+    private String itemType;
+    private int timeUntilExpire;
+
+
+    public FoodItem(String itemType, String ForegroundC, String BackGroundC) {
+        this.itemType = itemType;
+        this.timeUntilExpire = expiryDate(ForegroundC, BackGroundC, itemType);
+
+    }
+
+    private int expiryDate (String ForegroundC, String BackGroundC, String itemType) {
+        int expiryTime = 0;
+        if(itemType == "banana") {
+            if (ForegroundC == "green") {
+                expiryTime = 5;
+            }
+        }
+
+        return expiryTime;
+    }
+
+    public void decrementExpiryTime() {
+        timeUntilExpire--;
+    }
+
+
+
+}
