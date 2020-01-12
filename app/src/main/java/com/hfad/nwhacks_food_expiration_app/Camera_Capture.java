1bbc7e2ebe3b3fd6240969c10989382a3c90918c
jenileechen800 <chen.jenilee@gmail.com>
2020-01-11 17:58:49 -0800

    Working on 'File Save' to 'Camera Capture' Activity

--- app/src/main/java/com/hfad/nwhacks_food_expiration_app/Camera_Capture.java
+++ app/src/main/java/com/hfad/nwhacks_food_expiration_app/Camera_Capture.java
@@ -2,6 +2,7 @@ package com.hfad.nwhacks_food_expiration_app;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -10,10 +11,17 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Camera_Capture extends AppCompatActivity {
     ImageView view_photo;
@@ -22,8 +30,6 @@ public class Camera_Capture extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera__capture);
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
 
         view_photo = (ImageView) findViewById(R.id.imgview_photo);
     }
@@ -34,7 +40,22 @@ public class Camera_Capture extends AppCompatActivity {
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+                System.out.println(createImageFile().toString());
+            } catch (IOException ex) {
+                // error occured while creating file
+            }
+            // continue only if file was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.hfad.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
     }
 
@@ -46,12 +67,56 @@ public class Camera_Capture extends AppCompatActivity {
     // Get Image Thumbnail
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             view_photo.setImageBitmap(imageBitmap);
         }
     }
+
+    // Save photo
+    String currentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,     // prefix
+                ".jpg",     // suffix
+                storageDir         // directory
+        );
+
+        // Save a file: path for use with 'ACTION_VIEW' intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+//    private void dispatchTakePictureIntent() {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//
+//        // Ensure that there's a camera activity to handle the intent
+//
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            // Create File where photo should go
+//            File photoFile = null;
+//            try {
+//                photoFile = createImageFile();
+//            } catch (IOException ex) {
+//                // error occured while creating file
+//            }
+//            // continue only if file was successfully created
+//            if (photoFile != null) {
+//                Uri photoURI = FileProvider.getUriForFile(this,
+//                        "com.example.android.fileprovider",
+//                        photoFile);
+//                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+//                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+//            }
+//        }
+//    }
 
 }
--- app/src/main/java/com/hfad/nwhacks_food_expiration_app/MainActivity.java
+++ app/src/main/java/com/hfad/nwhacks_food_expiration_app/MainActivity.java
@@ -12,14 +12,11 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-//    ImageView view_photo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        view_photo = (ImageView) findViewById(R.id.imgview_photo2);
     }
 
 
@@ -27,32 +24,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Camera_Capture.class);
         startActivity(intent);
     }
-
-//    // Take Photo
-//    static final int REQUEST_IMAGE_CAPTURE = 1;
-//
-//    private void dispatchTakePictureIntent() {
-//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-//        }
-//    }
-//
-//    /** Called when user clicks the 'Take Photo' Button **/
-//    public void takePhoto(View view) {
-//        dispatchTakePictureIntent();
-//    }
-//
-//    // Get Image Thumbnail
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-////        super.onActivityResult(requestCode, resultCode, data);
-//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-//            Bundle extras = data.getExtras();
-//            Bitmap imageBitmap = (Bitmap) extras.get("data");
-//            view_photo.setImageBitmap(imageBitmap);
-//        }
-//    }
-
 
 }
