d084c44a49ca0d30e9fef537d24161e5667264fa
mpchow <mattchow918@gmail.com>
2020-01-11 16:32:55 -0800

    Updated FoodItem class

--- app/src/main/java/com/hfad/nwhacks_food_expiration_app/FoodItem.java
+++ app/src/main/java/com/hfad/nwhacks_food_expiration_app/FoodItem.java
@@ -1,14 +1,26 @@
 package com.hfad.nwhacks_food_expiration_app;
 
+import java.util.*;
+
 public class FoodItem {
+
     private String itemType;
     private int timeUntilExpire;
 
 
-    public FoodItem(String itemType, String ForegroundC, String BackGroundC) {
-        this.itemType = itemType;
+    public FoodItem(String itemType, String ForegroundC, String BackGroundC, List<String> itemsInPicture, Set<String> validItems) throws InstantiationException {
+        this.itemType = assignItem(itemsInPicture, validItems);
         this.timeUntilExpire = expiryDate(ForegroundC, BackGroundC, itemType);
 
+    }
+
+    private String assignItem(List<String> itemsInPicture, Set<String> validItems) throws InstantiationException {
+        for (String s : itemsInPicture) {
+            if (validItems.contains(s)) {
+                return s;
+            }
+        }
+        throw new InstantiationException();
     }
 
     private int expiryDate (String ForegroundC, String BackGroundC, String itemType) {
